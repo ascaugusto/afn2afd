b954e1c46c094f78253c6ee180e6cc2aa7972781
Augusto Coelho <ascaugusto@gmail.com>
2017-03-22 19:53:19 -0300

    Alteracao de principal e adc arquivo de entrada

--- afn2afd.py
+++ afn2afd.py
@@ -1,8 +1,26 @@
 
 nom_arquivos = input("Digite o nome do arquivo de entrada e em seguida o de saida:\n")
- 
+
+ if nom_arquivos.count(' ') == 0:
+	nom_arq1 = nom_arquivos
+	list1 = nom_arq1.split('.')
+	nom_arq2 = list[0]+'.afd'
+	print(nom_arq2) 
+else:
+	list2 = nom_arquivos.split(' ')
+    nom_arq1 = list2[0]
+    nom_arq2 = list2[1]
+
+print(nom_arq1) 
+print(nom_arq2) 
+
+arq_entrada = open(nom_arq1,'r')
+arq1_conteudo = arq_entrada.read()
+
+print(arq1_conteudo) 
 
 
+input("press")
 
 
 
