574986680a9ecb712ba7e0ee1a61f10c8067bee7
Augusto Coelho <ascaugusto@gmail.com>
2017-03-23 19:15:04 -0300

    Correcao do IF

--- afn2afd.py
+++ afn2afd.py
@@ -1,13 +1,15 @@
 
 nom_arquivos = input("Digite o nome do arquivo de entrada e em seguida o de saida:\n")
 
- if nom_arquivos.count(' ') == 0:
-	nom_arq1 = nom_arquivos
-	list1 = nom_arq1.split('.')
-	nom_arq2 = list[0]+'.afd'
-	print(nom_arq2) 
+qt_espaco = nom_arquivos.count(' ')
+
+if qt_espaco == 0:
+    nom_arq1 = nom_arquivos
+    list1 = nom_arq1.split('.')
+    nom_arq2 = list1[0]+'.afd'
+    print(nom_arq2)
 else:
-	list2 = nom_arquivos.split(' ')
+    list2 = nom_arquivos.split(' ')
     nom_arq1 = list2[0]
     nom_arq2 = list2[1]
 
