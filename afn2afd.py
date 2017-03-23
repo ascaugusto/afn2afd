e4f148d8842d750d3a18acc98390a93075fa2161
Augusto Coelho <ascaugusto@gmail.com>
2017-03-23 19:23:21 -0300

    Update

--- afn2afd.py
+++ afn2afd.py
@@ -7,20 +7,16 @@ if qt_espaco == 0:
     nom_arq1 = nom_arquivos
     list1 = nom_arq1.split('.')
     nom_arq2 = list1[0]+'.afd'
-    print(nom_arq2)
 else:
     list2 = nom_arquivos.split(' ')
     nom_arq1 = list2[0]
     nom_arq2 = list2[1]
 
-print(nom_arq1) 
-print(nom_arq2) 
-
 arq_entrada = open(nom_arq1,'r')
 arq1_conteudo = arq_entrada.read()
 
-print(arq1_conteudo) 
 
+print(arq1_conteudo) 
 
 input("press")
 
